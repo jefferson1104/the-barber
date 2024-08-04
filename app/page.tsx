9035ead1f31d021fdd992fb4cebbe29fa6fcf145
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2024-08-04 08:33:12 -0300

    chore: add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
+"use client"
+
+import { Button } from "./_components/ui/button"
 
 export default function Home() {
   return (
-    <h1 className="text-orange-400 bg-slate-200 p-4 mt-2 ">Home Page</h1>
+    <div>
+      <h1 className="text-orange-400 bg-slate-200 p-4 mt-2 ">Home Page</h1>
+      <Button>Click Me</Button>
+    </div>
   );
 }
