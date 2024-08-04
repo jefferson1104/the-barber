ecb88fe078d97d30e857868057fa9507a15a0f6b
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2024-08-04 08:43:19 -0300

    chore: add git hook

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import { Button } from "./_components/ui/button"
 export default function Home() {
   return (
     <div>
-      <h1 className="text-orange-400 bg-slate-200 p-4 mt-2 ">Home Page</h1>
+      <h1 className="mt-2 bg-slate-200 p-4 text-orange-400">Home Page</h1>
       <Button>Click Me</Button>
     </div>
-  );
+  )
 }
