3754b3d6415ba4f321e721788547010039f51d63
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2024-09-14 22:47:02 -0300

    fix: implementing bookings desktop version

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -8,6 +8,13 @@ import { getFinalizedBookings } from "../_data/get-finalized-bookings"
 
 import { Header } from "../_components/header"
 import { BookingItem } from "../_components/booking-item"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../_components/ui/carousel"
 
 const Bookings = async () => {
   // Constants
@@ -21,50 +28,107 @@ const Bookings = async () => {
   }
 
   return (
-    <>
-      <Header />
+    <section>
+      {/* Mobile Version */}
+      <div className="lg:hidden">
+        <Header />
+        <div className="space-y-3 p-5">
+          <h1 className="text-xl font-bold">Bookings</h1>
 
-      <div className="space-y-3 p-5">
-        <h1 className="text-xl font-bold">Bookings</h1>
+          {/* Empty Bookings */}
+          {confirmedBookings.length === 0 && finalizedBookings.length === 0 && (
+            <div className="mt-6 text-center text-gray-400">
+              No bookings found
+            </div>
+          )}
 
-        {/* Empty Bookings */}
-        {confirmedBookings.length === 0 && finalizedBookings.length === 0 && (
-          <div className="mt-6 text-center text-gray-400">
-            No bookings found
-          </div>
-        )}
+          {/* Confirmed Bookings */}
+          {confirmedBookings.length > 0 && (
+            <>
+              <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+                Confirmed
+              </h2>
+              {confirmedBookings.map((booking) => (
+                <BookingItem
+                  key={booking.id}
+                  booking={JSON.parse(JSON.stringify(booking))}
+                />
+              ))}
+            </>
+          )}
 
-        {/* Confirmed Bookings */}
-        {confirmedBookings.length > 0 && (
-          <>
-            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-              Confirmed
-            </h2>
-            {confirmedBookings.map((booking) => (
-              <BookingItem
-                key={booking.id}
-                booking={JSON.parse(JSON.stringify(booking))}
-              />
-            ))}
-          </>
-        )}
-
-        {/* Finalized Bookings */}
-        {finalizedBookings.length > 0 && (
-          <>
-            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-              Finalized
-            </h2>
-            {finalizedBookings.map((booking) => (
-              <BookingItem
-                key={booking.id}
-                booking={JSON.parse(JSON.stringify(booking))}
-              />
-            ))}
-          </>
-        )}
+          {/* Finalized Bookings */}
+          {finalizedBookings.length > 0 && (
+            <>
+              <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+                Finalized
+              </h2>
+              {finalizedBookings.map((booking) => (
+                <BookingItem
+                  key={booking.id}
+                  booking={JSON.parse(JSON.stringify(booking))}
+                />
+              ))}
+            </>
+          )}
+        </div>
       </div>
-    </>
+
+      {/* Desktop Version */}
+      <div className="hidden lg:contents">
+        <Header />
+        <div className="mt-12 space-y-3 px-32">
+          <h1 className="text-xl font-bold">Bookings</h1>
+
+          {/* Empty Bookings */}
+          {confirmedBookings.length === 0 && finalizedBookings.length === 0 && (
+            <div className="mt-6 text-left text-gray-400">
+              No confirmed bookings found.
+            </div>
+          )}
+
+          {/* Confirmed Bookings */}
+          {confirmedBookings.length > 0 && (
+            <div className="max-w-[1380px] space-y-3 px-16">
+              <h2 className="mt-6 text-xs font-bold uppercase text-gray-400">
+                Confirmed
+              </h2>
+              <Carousel opts={{ align: "start", inViewThreshold: 1 }}>
+                <CarouselContent className="cursor-pointer lg:w-[420px]">
+                  {confirmedBookings.map((booking) => (
+                    <CarouselItem key={booking.id}>
+                      <BookingItem
+                        booking={JSON.parse(JSON.stringify(booking))}
+                      />
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+                <CarouselPrevious className="ml-5 h-12 w-12" />
+                <CarouselNext className="mr-5 h-12 w-12" />
+              </Carousel>
+            </div>
+          )}
+
+          {/* Finalized Bookings */}
+          {finalizedBookings.length > 0 && (
+            <div className="space-y-3 px-16 py-4">
+              <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+                Finalized
+              </h2>
+              <div className="flex flex-wrap gap-4">
+                {finalizedBookings.map((booking) => (
+                  <div key={booking.id} className="w-[405px]">
+                    <BookingItem
+                      booking={JSON.parse(JSON.stringify(booking))}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
   )
 }
 
